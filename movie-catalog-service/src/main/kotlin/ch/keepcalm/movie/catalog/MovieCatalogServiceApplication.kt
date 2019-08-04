b9f1817c3e7fc2abfc4c2e48bbebbbd6dc412e35
Marcel Widmer <marzelwidmer@gmail.com>
2019-08-04 08:06:47 +0200

    Polishing.

--- movie-catalog-service/src/main/kotlin/ch/keepcalm/movie/catalog/MovieCatalogServiceApplication.kt
+++ movie-catalog-service/src/main/kotlin/ch/keepcalm/movie/catalog/MovieCatalogServiceApplication.kt
@@ -38,26 +38,27 @@ class CatalogService(private val repository: CatalogRepository) {
 
 }
 
-interface CatalogRepository : ReactiveCrudRepository<CatalogItem, String>
-
-@Document
-data class CatalogItem(@Id val id: String = UUID.randomUUID().toString(), val name: String, val desc: String, val rating: Double = 0.0)
-
 @Component
 class DataLoader(private val repository: CatalogRepository) {
 
     @PostConstruct
     fun load() =
-            repository.deleteAll().thenMany(
-                    listOf(
-                            CatalogItem(name = "Fast & Furious Presents: Hobbs & Shaw", desc = "Lawman Luke Hobbs and outcast Deckard Shaw form an unlikely alliance when a cyber-genetically enhanced villain threatens the future of humanity.", rating = 7.2),
-                            CatalogItem(name = "The Nightingale", desc = "Set in 1825, Clare, a young Irish convict woman, chases a British officer through the rugged Tasmanian wilderness, bent on revenge for a terrible act of violence he committed against her family. On the way she enlists the services of an Aboriginal tracker named Billy, who is also marked by trauma from his own violence-filled past.", rating = 6.5),
-                            CatalogItem(name = "Luce", desc = "A married couple is forced to reckon with their idealized image of their son, adopted from war-torn Eritrea, after an alarming discovery by a devoted high school teacher threatens his status as an all-star student.", rating = 6.6),
-                            CatalogItem(name = "Them That Follow", desc = "Set deep in the wilds of Appalachia, where believers handle death-dealing snakes to prove themselves before God, Them That Follow tells the story of a pastor's daughter who holds a secret that threatens to tear her community apart.", rating = 5.5),
-                            CatalogItem(name = "La paranza dei bambini", desc = "A gang of teenage boys stalk the streets of Naples armed with hand guns and AK-47s to do their mob bosses' bidding.", rating = 6.5))
-                            .toFlux()
-                            .map { CatalogItem(name = it.name, desc = it.desc, rating = it.rating) }
-                            .flatMap { repository.save(it) }
-                            .thenMany(repository.findAll())
-            ).subscribe { print(it) }
+            repository.deleteAll()
+                    .thenMany(
+                            listOf(
+                                    CatalogItem(name = "Fast & Furious Presents: Hobbs & Shaw", desc = "Lawman Luke Hobbs and outcast Deckard Shaw form an unlikely alliance when a cyber-genetically enhanced villain threatens the future of humanity.", rating = 7.2),
+                                    CatalogItem(name = "The Nightingale", desc = "Set in 1825, Clare, a young Irish convict woman, chases a British officer through the rugged Tasmanian wilderness, bent on revenge for a terrible act of violence he committed against her family. On the way she enlists the services of an Aboriginal tracker named Billy, who is also marked by trauma from his own violence-filled past.", rating = 6.5),
+                                    CatalogItem(name = "Luce", desc = "A married couple is forced to reckon with their idealized image of their son, adopted from war-torn Eritrea, after an alarming discovery by a devoted high school teacher threatens his status as an all-star student.", rating = 6.6),
+                                    CatalogItem(name = "Them That Follow", desc = "Set deep in the wilds of Appalachia, where believers handle death-dealing snakes to prove themselves before God, Them That Follow tells the story of a pastor's daughter who holds a secret that threatens to tear her community apart.", rating = 5.5),
+                                    CatalogItem(name = "La paranza dei bambini", desc = "A gang of teenage boys stalk the streets of Naples armed with hand guns and AK-47s to do their mob bosses' bidding.", rating = 6.5)
+                            ).toFlux()
+                                    .map { CatalogItem(name = it.name, desc = it.desc, rating = it.rating) }
+                                    .flatMap { repository.save(it) }
+                                    .thenMany(repository.findAll())
+                    ).subscribe { print(it) }
 }
+
+interface CatalogRepository : ReactiveCrudRepository<CatalogItem, String>
+
+@Document
+data class CatalogItem(@Id val id: String = UUID.randomUUID().toString(), val name: String, val desc: String, val rating: Double = 0.0)
--- movie-info-service/src/main/kotlin/ch/keepcalm/movie/info/MovieInfoServiceApplication.kt
+++ movie-info-service/src/main/kotlin/ch/keepcalm/movie/info/MovieInfoServiceApplication.kt
@@ -2,10 +2,41 @@ package ch.keepcalm.movie.info
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.stereotype.Component
+import reactor.kotlin.core.publisher.toFlux
+import java.util.*
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
 class MovieInfoServiceApplication
 
 fun main(args: Array<String>) {
-	runApplication<MovieInfoServiceApplication>(*args)
+    runApplication<MovieInfoServiceApplication>(*args)
 }
+
+
+@Component
+class DataLoader(private val repository: MovieRepository) {
+
+    @PostConstruct
+    fun load() = repository.deleteAll()
+            .thenMany( // When it finish then ...
+                    listOf(
+                            Movie(name = "Fast & Furious Presents: Hobbs & Shaw"),
+                            Movie(name = "The Nightingale"),
+                            Movie(name = "Luce"),
+                            Movie(name = "Them That Follow"),
+                            Movie(name = "La paranza dei bambini")
+                    ).toFlux() // Convert List to a Flux Object
+                            .map { Movie(name = it.name) } // [a, b, c] f(x) => [f(a), f(b), f(c)]
+                            .flatMap { repository.save(it) } // [[a, b] ,[c, d] f(x) => [f(a), f(b), f(c), f(d)]
+                            .thenMany(repository.findAll()) // Search entries when it is finish...
+            ).subscribe { print(it) } // Then print it out...
+}
+
+interface MovieRepository : ReactiveCrudRepository<Movie, String>
+
+@Document
+data class Movie(val id: String = UUID.randomUUID().toString(), val name: String)
