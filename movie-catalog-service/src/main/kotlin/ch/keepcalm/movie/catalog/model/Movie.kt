25810149b6e6811ded5e89b3bc9556b1ed6c037c
Marcel Widmer <marzelwidmer@gmail.com>
2019-09-02 22:59:49 +0200

    Clean repository

--- movie-catalog-service/src/main/kotlin/ch/keepcalm/movie/catalog/model/Movie.kt
+++
@@ -1,8 +0,0 @@
-package ch.keepcalm.movie.catalog.model
-
-import org.springframework.data.annotation.Id
-import org.springframework.data.mongodb.core.mapping.Document
-import java.util.*
-
-@Document
-data class Movie(@Id val id: String = UUID.randomUUID().toString(), val name: String, val desc: String)
